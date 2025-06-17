452f476a20e20fd4331c4aa5a0e1cde32a7c3f8d
francescoboschelle <francibossch@gmail.com>
2025-06-17 11:29:34 +0200

    Refactor grid layout in style.css and update column classes in main.js for responsive design

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ fetch(endpoint)
   .then((res) => res.json())
   .then((data) => {
     data.forEach((post) => {
-      rowEl.innerHTML += `<div class="col-12">
+      rowEl.innerHTML += `<div class="col-12 col-md-6 col-xl-4">
             <div class="card">
               <img src="./img/pin.svg" alt="Pin" class="pin" />
               <img src="${post.url}" alt="Post Image" class="mb-1" />
