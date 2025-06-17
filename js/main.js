f42c1fa03a2e4e6c92d9eb2e9e98ceacf67c3511
francescoboschelle <francibossch@gmail.com>
2025-06-17 11:21:57 +0200

    Implement dynamic content loading in main.js and clean up index.html structure

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
+const rowEl = document.querySelector(".row");
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((post) => {
+      rowEl.innerHTML += `<div class="col-12">
+            <div class="card">
+              <img src="./img/pin.svg" alt="Pin" class="pin" />
+              <img src="${post.url}" alt="Post Image" class="mb-1" />
+              <p>${post.title}</p>
+            </div>
+          </div>`;
+    });
+  });
